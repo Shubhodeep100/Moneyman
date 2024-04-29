0ffa6a1af78da2024c5ec87836805b1570d89693
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-29 21:39:15 +0530

    Logo Setup

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PiggyBank } from 'lucide-react';
+function Logo() {
+  return (
+   <a href="/" className='flex items-center gap-2'>
+    <PiggyBank className="stroke"/>
+   </a>
+  )
+}
+
+export default Logo;
