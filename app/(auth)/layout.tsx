102b176799f4c5f1dbc93655d42ba810290fdc96
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-30 14:32:07 +0530

    Shadcn setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
+import Logo from '@/components/Logo'
 import React, { ReactNode } from 'react'
 
 function layout({ children }: { children: ReactNode }) {
     return (
-        <div className='relative flex h-screen w-full flex-col items-center justify-center'>
-            <div className='mt-12'>{children}</div>
+        <div className='relative flex h-screen w-full flex-col items-center justify-center bg-black'>
+
+
+            <Logo />
+            <div className='mt-4'>{children}</div>
+
         </div>
     )
 }
