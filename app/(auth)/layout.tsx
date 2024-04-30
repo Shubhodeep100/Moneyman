1de7cb9f55c1c52dacefeed45f5538e867a79fe2
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-30 14:50:24 +0530

    Next themes setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,11 +4,8 @@ import React, { ReactNode } from 'react'
 function layout({ children }: { children: ReactNode }) {
     return (
         <div className='relative flex h-screen w-full flex-col items-center justify-center bg-black'>
-
-
             <Logo />
             <div className='mt-4'>{children}</div>
-
         </div>
     )
 }
