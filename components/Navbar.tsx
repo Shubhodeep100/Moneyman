57bb6f18ce8f9106a3a13a465a6f89e4e52183e0
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-01 11:42:07 +0530

    Dashboard routes additions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react'
 import Logo from './Logo'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
 
 function Navbar() {
     return (
@@ -9,6 +13,11 @@ function Navbar() {
     )
 }
 
+const items = [
+    { label: "Dashboard", link: "/" },
+    { label: "Transactions", link: "/transactions" },
+    { label: "Manage", link: "/manage" },
+]
 
 function DesktopNavbar() {
     return (
@@ -16,10 +25,28 @@ function DesktopNavbar() {
             <nav className='container flex items-center justify-between px-8'>
                 <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
                     <Logo />
-                    <div className='flex h-full'></div>
+                    <div className='flex h-full'>
+                        {items.map(item => (
+                            <NavbarItem key={item.label}
+                                link={item.link}
+                                label={item.label}
+                            />
+                        ))}
+                    </div>
                 </div>
             </nav>
         </div>
     )
+}
+
+function NavbarItem({ link, label }: {
+    link: string,
+    label: string,
+}) {
+    const pathname = usePathname();
+    const isActive = pathname === link;
+    return <div className='relative flex items-center'>
+        <Link href={link}>{label}</Link>
+    </div>
 }
 export default Navbar
