ee1c9b0005d157dd44a25ffe4b0051c31b757b41
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-01 11:33:24 +0530

    Dashboard Work

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,13 @@
+import Navbar from '@/components/Navbar'
 import React, { ReactNode } from 'react'
 
 function layout({ children }: { children: ReactNode }) {
     return (
         <div className='relative flex h-screen w-full flex-col'>
-            {children}
+            <Navbar />
+            <div className='w-full'>
+                {children}
+            </div>
         </div>
     )
 }
