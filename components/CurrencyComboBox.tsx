49a9761f8301c77b7b38cbec7264c858dccbf125
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-03 12:43:19 +0530

    Currency setup in wizard page

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -22,39 +22,13 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import { Currencies, Currency } from "@/lib/currencies"
 
-type Status = {
-    value: string
-    label: string
-}
-
-const statuses: Status[] = [
-    {
-        value: "backlog",
-        label: "Backlog",
-    },
-    {
-        value: "todo",
-        label: "Todo",
-    },
-    {
-        value: "in progress",
-        label: "In Progress",
-    },
-    {
-        value: "done",
-        label: "Done",
-    },
-    {
-        value: "canceled",
-        label: "Canceled",
-    },
-]
 
 export function CurrencyComboBox() {
     const [open, setOpen] = React.useState(false)
     const isDesktop = useMediaQuery("(min-width: 768px)")
-    const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
+    const [selectedOption, setSelectedOption] = React.useState<Currency | null>(
         null
     )
 
@@ -63,11 +37,11 @@ export function CurrencyComboBox() {
             <Popover open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
                     <Button variant="outline" className="w-[150px] justify-start">
-                        {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+                        {selectedOption ? <>{selectedOption.label}</> : <>+ Set status</>}
                     </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-[200px] p-0" align="start">
-                    <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+                  <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption}/>
                 </PopoverContent>
             </Popover>
         )
@@ -77,43 +51,43 @@ export function CurrencyComboBox() {
         <Drawer open={open} onOpenChange={setOpen}>
             <DrawerTrigger asChild>
                 <Button variant="outline" className="w-[150px] justify-start">
-                    {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+                    {selectedOption ? <>{selectedOption.label}</> : <>+ Set status</>}
                 </Button>
             </DrawerTrigger>
             <DrawerContent>
                 <div className="mt-4 border-t">
-                    <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+                    <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
                 </div>
             </DrawerContent>
         </Drawer>
     )
 }
 
-function StatusList({
+function OptionList({
     setOpen,
-    setSelectedStatus,
+    setSelectedOption,
 }: {
     setOpen: (open: boolean) => void
-    setSelectedStatus: (status: Status | null) => void
+    setSelectedOption: (status: Currency | null) => void
 }) {
     return (
         <Command>
-            <CommandInput placeholder="Filter status..." />
+            <CommandInput placeholder="Filter currency..." />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
                 <CommandGroup>
-                    {statuses.map((status) => (
+                    {Currencies.map((currency: Currency) => (
                         <CommandItem
-                            key={status.value}
-                            value={status.value}
+                            key={currency.value}
+                            value={currency.value}
                             onSelect={(value) => {
-                                setSelectedStatus(
-                                    statuses.find((priority) => priority.value === value) || null
+                                setSelectedOption(
+                                    Currencies.find((priority) => priority.value === value) || null
                                 )
                                 setOpen(false)
                             }}
                         >
-                            {status.label}
+                            {currency.label}
                         </CommandItem>
                     ))}
                 </CommandGroup>
