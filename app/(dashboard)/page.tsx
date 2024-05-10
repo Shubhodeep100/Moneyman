2928c237bf7a377bf1a37f3623ff425ff8257b32
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-10 18:37:14 +0530

    Toaste updates

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='bg-zinc-900 h-screen flex justify-center items-center'>
+    <div className=' h-screen flex justify-center items-center'>
       This is The Dashboard.
     </div>
   )
