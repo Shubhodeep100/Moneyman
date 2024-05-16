7b6a01a3a703cc4521e2ecb66584381f16364a20
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-16 07:14:28 +0530

    Dialog Box work

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { DialogTitle } from "@radix-ui/react-dialog";
 import { ReactNode } from "react";
 
 interface Props {
@@ -17,6 +19,11 @@ function CreateTransactionDialog({ trigger, type }: Props) {
             <DialogTrigger asChild>
                 {trigger}
             </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create a new <span className={cn("m-1", type === "income" ? "text-emerald-500" : "text-red-500")}>{type}</span></DialogTitle>
+                </DialogHeader>
+            </DialogContent>
         </Dialog >
     );
 }
