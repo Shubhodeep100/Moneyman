4346002cd1bff1ccd744c47b110916818611d157
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-16 07:03:52 +0530

    Dashboard work update

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,6 +1,24 @@
 "use client"
 
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { ReactNode } from "react";
+
 interface Props {
     trigger: ReactNode;
-    type:
+    type: TransactionType;
 }
+
+import React from "react"
+
+function CreateTransactionDialog({ trigger, type }: Props) {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                {trigger}
+            </DialogTrigger>
+        </Dialog >
+    );
+}
+
+export default CreateTransactionDialog;
