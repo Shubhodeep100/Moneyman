71653331b70b461d57fe1d531ae6f08118d4eb68
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-16 07:18:01 +0530

    Zod schema Build

---
+++ schema/transaction.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const CreateTransactionSchema = z.object({
+  amount: z.coerce.number().positive().multipleOf(0.01),
+  description: z.string(),
+  date: z.coerce.date(),
+  category: z.string(),
+});
