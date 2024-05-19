114570c17e24519054c420ea0676635769aee976
Shubhodeep <banerjeeshubho98@gmail.com>
2024-05-19 23:56:44 +0530

    Form bug fix

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -17,6 +17,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import CategoryPicker from "./CategoryPicker";
 
 function CreateTransactionDialog({ trigger, type }: Props) {
     const form = useForm<CreateTrasactionScemaType>({
@@ -45,14 +46,44 @@ function CreateTransactionDialog({ trigger, type }: Props) {
                                 <FormItem>
                                     <FormLabel>Description</FormLabel>
                                     <FormControl>
-                                        <Input {...field} />
+                                        <Input defaultValue={""} {...field} />
                                     </FormControl>
                                     <FormDescription>Transaction Description (optional)</FormDescription>
                                 </FormItem>
                             )}
                         />
+
+                        <FormField
+                            control={form.control}
+                            name="amount"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Amount</FormLabel>
+                                    <FormControl>
+                                        <Input defaultValue={0} type="number" {...field} />
+                                    </FormControl>
+                                    <FormDescription>Transaction Amount (required)</FormDescription>
+                                </FormItem>
+                            )}
+                        />
                         <button type="submit">Submit</button>
                     </form>
+
+                    <div className="flex items-center justify-between gap-2" >
+                        <FormField
+                            control={form.control}
+                            name="category"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Category</FormLabel>
+                                    <FormControl>
+                                        <CategoryPicker type={type}/>
+                                    </FormControl>
+                                    <FormDescription>Select a category for this transaction</FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
                 </FormProvider>
             </DialogContent>
         </Dialog>
